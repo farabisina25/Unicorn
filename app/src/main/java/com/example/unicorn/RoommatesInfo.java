5e17353876056dc1823c5da0319aa3b89e188ff5
Farabi Sina Sari <farabisina@live.com>
2023-05-19 17:23:01 +0300

    Version 4.0

--- app/src/main/java/com/example/unicorn/RoommatesInfo.java
+++ app/src/main/java/com/example/unicorn/RoommatesInfo.java
@@ -113,6 +113,7 @@ public class RoommatesInfo extends AppCompatActivity {
         editText1 = findViewById(R.id.editTextText6);
         editText2 = findViewById(R.id.editTextText7);
 
+        //comment
         //isRoommateCreated();
 
         profileBtn.setOnClickListener(new View.OnClickListener() {
